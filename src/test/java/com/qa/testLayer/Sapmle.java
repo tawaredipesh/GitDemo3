9182f6ffcc8dd16aa2427126f73a0a5c88481407
Vaishali <tawaredipesh@gmail.com>
2022-09-19 17:20:35 +0530

    this is Demo3

--- src/test/java/com/qa/testLayer/Sapmle.java
+++ src/test/java/com/qa/testLayer/Sapmle.java
@@ -5,6 +5,8 @@ public class Sapmle {
 	public static void main(String[] args) {
 	System.out.println("Hello. This is from Emma");
 	System.out.println("Hello. This is from Dipesh. second commit");
+	
+	System.out.println("First commit");
 	}
 
 }
